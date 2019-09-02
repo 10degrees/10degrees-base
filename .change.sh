a410e20cbd0e00c8056b442ab5a796d7a31819c6
Matt Radford <matt@10degrees.uk>
2019-09-02 15:24:38 +0100

    Track change script

---
+++ .change.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo -n "New text domain: "
+read textdomain
+echo -n "New theme name: "
+read themename
+echo -n "\nChanging textdomain & theme name in:\n"
+grep -rl "@textdomain" *.css -R
+echo -n "\nChanging textdomain in:\n"
+grep -rl "@textdomain" **/*.php -R
+grep -rl "@textdomain" *.css -R | xargs sed -i "" 's/@textdomain/'$textdomain'/'
