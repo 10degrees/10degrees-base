52571ca55fe5a3250aa0b640c4d85fc2fc6e570c
Karl Maw <karl@10degrees.uk>
2019-09-04 12:29:20 +0100

    BT10D-37

--- app/Boot/CleanUp.php
+++ app/Boot/CleanUp.php
@@ -2,47 +2,54 @@
 
 namespace App\Boot;
 
+/**
+ * Remove bloat
+ * 
+ * @category Cleanup
+ * @package  TenDegrees/Base
+ * @author   10 Degrees <hello@10degrees.uk>
+ * @license  https://opensource.org/licenses/MIT MIT 
+ * @link     https://www.10degrees.uk
+ */
 class CleanUp
 {
+    /**
+     * Initialize the object
+     */
     public function __construct()
     {
-        add_action( 'after_setup_theme', [$this, 'cleanUpHead'] );
-        add_action( 'init',  [$this, 'disable_emojis']);
+        remove_action('wp_head', 'print_emoji_detection_script', 7);
+        remove_action('wp_print_styles', 'print_emoji_styles');
+        remove_action('wp_head', 'wlwmanifest_link');
+        remove_action('wp_head', 'wp_generator');
+        remove_action('wp_head', 'rsd_link');
+        remove_action('wp_head', 'print_emoji_detection_script', 7);
+        remove_action('admin_print_scripts', 'print_emoji_detection_script');
+        remove_action('wp_print_styles', 'print_emoji_styles');
+        remove_action('admin_print_styles', 'print_emoji_styles'); 
+        remove_filter('the_content_feed', 'wp_staticize_emoji');
+        remove_filter('comment_text_rss', 'wp_staticize_emoji'); 
+        remove_filter('wp_mail', 'wp_staticize_emoji_for_email');
+        add_filter('feed_links_show_comments_feed', '__return_false'); 
+        add_filter('tiny_mce_plugins', [$this, 'disableEmojisTinyMce']);
+        add_filter('wp_resource_hints', [$this, 'disableEmojiDnsPrefetch'], 10, 2);
     }
 
-    public function cleanUpHead()
-    {
-        remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
-        remove_action( 'wp_print_styles', 'print_emoji_styles' );
-        remove_action( 'wp_head', 'rsd_link' );
-        remove_action( 'wp_head', 'wlwmanifest_link' );
-        remove_action( 'wp_head', 'wp_generator' );
-
-        // Disable comments feed, WP 4.4 onwards
-        add_filter( 'feed_links_show_comments_feed', '__return_false' ); 
-    }
-
-    public function disable_emojis() 
-    {
-        remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
-        remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
-        remove_action( 'wp_print_styles', 'print_emoji_styles' );
-        remove_action( 'admin_print_styles', 'print_emoji_styles' ); 
-        remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
-        remove_filter( 'comment_text_rss', 'wp_staticize_emoji' ); 
-        remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
-        add_filter( 'tiny_mce_plugins', 'disableEmojisTinyMce' );
-        add_filter( 'wp_resource_hints', 'disableEmojiDnsPrefetch', 10, 2 );
-    }
-
+    /**
+     * Filter emojis out of TinyMCE
+     *
+     * @param array $plugins 
+     * 
+     * @return void
+     */
     public function disableEmojisTinyMce( $plugins ) 
     {
 
-        if ( is_array( $plugins ) ) {
+        if (is_array($plugins)) {
 
-            return array_diff( $plugins, array( 'wpemoji' ) );
+            return array_diff($plugins, array( 'wpemoji' ));
 
-            } else {
+        } else {
 
             return array();
 
@@ -50,14 +57,24 @@ class CleanUp
 
     }
 
+    /**
+     * Disable emoji DNS prefetch
+     *
+     * @param array  $urls          URLs to prefetch
+     * @param string $relation_type type of DNS hint
+     * 
+     * @return void
+     */
     public function disableEmojiDnsPrefetch( $urls, $relation_type ) 
     {
 
         if ('dns-prefetch' == $relation_type) {
 
-            $emoji_svg_url = apply_filters( 'emoji_svg_url', 'https://s.w.org/images/core/emoji/2/svg/' );
+            $emoji_url = 'https://s.w.org/images/core/emoji/2/svg/';
 
-            $urls = array_diff( $urls, array( $emoji_svg_url ) );
+            $emoji_svg_url = apply_filters('emoji_svg_url', $emoji_url);
+
+            $urls = array_diff($urls, array($emoji_svg_url));
 
         }
 
