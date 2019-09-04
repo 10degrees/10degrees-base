b96ad351c2091c7ca328b5710bc10351a831c78e
Karl Maw <karl@10degrees.uk>
2019-09-04 11:57:39 +0100

    BT10D-37

--- app/Boot/CleanUp.php
+++ app/Boot/CleanUp.php
@@ -4,21 +4,65 @@ namespace App\Boot;
 
 class CleanUp
 {
-	public function __construct()
-	{
-		add_action( 'after_setup_theme', [$this, 'cleanUpHead'] );
-	}
+    public function __construct()
+    {
+        add_action( 'after_setup_theme', [$this, 'cleanUpHead'] );
+        add_action( 'init',  [$this, 'disable_emojis']);
+    }
 
-	public function cleanUpHead()
-	{
-	    remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
-	    remove_action( 'wp_print_styles', 'print_emoji_styles' );
-	    remove_action( 'wp_head', 'rsd_link' );
-	    remove_action( 'wp_head', 'wlwmanifest_link' );
-	    remove_action( 'wp_head', 'wp_generator' );
+    public function cleanUpHead()
+    {
+        remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+        remove_action( 'wp_print_styles', 'print_emoji_styles' );
+        remove_action( 'wp_head', 'rsd_link' );
+        remove_action( 'wp_head', 'wlwmanifest_link' );
+        remove_action( 'wp_head', 'wp_generator' );
 
-	    // Disable comments feed, WP 4.4 onwards
-	    add_filter( 'feed_links_show_comments_feed', '__return_false' ); 
-	}
-	
+        // Disable comments feed, WP 4.4 onwards
+        add_filter( 'feed_links_show_comments_feed', '__return_false' ); 
+    }
+
+    public function disable_emojis() 
+    {
+        remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+        remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+        remove_action( 'wp_print_styles', 'print_emoji_styles' );
+        remove_action( 'admin_print_styles', 'print_emoji_styles' ); 
+        remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+        remove_filter( 'comment_text_rss', 'wp_staticize_emoji' ); 
+        remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+        add_filter( 'tiny_mce_plugins', 'disableEmojisTinyMce' );
+        add_filter( 'wp_resource_hints', 'disableEmojiDnsPrefetch', 10, 2 );
+    }
+
+    public function disableEmojisTinyMce( $plugins ) 
+    {
+
+        if ( is_array( $plugins ) ) {
+
+            return array_diff( $plugins, array( 'wpemoji' ) );
+
+            } else {
+
+            return array();
+
+        }
+
+    }
+
+    public function disableEmojiDnsPrefetch( $urls, $relation_type ) 
+    {
+
+        if ('dns-prefetch' == $relation_type) {
+
+            $emoji_svg_url = apply_filters( 'emoji_svg_url', 'https://s.w.org/images/core/emoji/2/svg/' );
+
+            $urls = array_diff( $urls, array( $emoji_svg_url ) );
+
+        }
+
+        return $urls;
+
+    }
+
 }
