6e27a00e65f2c557dab44063cd96e047f0dcc0d0
Matt Radford <matt@10degrees.uk>
2019-10-28 15:23:48 +0000

    Replace functions in head.php

--- partials/head.php
+++ partials/head.php
@@ -3,5 +3,5 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php wp_head(); ?>
-    <link rel="alternate" type="application/rss+xml" title="<?php echo esc_html( get_bloginfo( 'name' ) ); ?> Feed" href="<?php echo esc_url( get_feed_link() ); ?>">
+    <link rel="alternate" type="application/rss+xml" title="<?php esc_html_e(get_bloginfo('name')); ?> Feed" href="<?php echo esc_url(get_feed_link()); ?>">
 </head>
