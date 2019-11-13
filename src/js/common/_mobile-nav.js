3f6675344b44922e1991e56bdcc7a911a4bdc21c
Alice Williams <alice@10degrees.uk>
2019-11-13 11:01:51 +0000

    Add dropdown styling.

--- src/js/common/_mobile-nav.js
+++ src/js/common/_mobile-nav.js
@@ -1,7 +1,26 @@
 class MobileNav {
 	init() {
+		this.addNavigationToggle();
+		this.addDropdownToggle();
+	}
+
+	addNavigationToggle(){
 		$('.header .toggle').click(function() {
 			$('#nav-primary').toggleClass('-open');
+		});
+	}
+
+	addDropdownToggle(){
+		$('.menu-item.dropdown').click(function(e) {
+			e.preventDefault();
+
+			$(this).find('> .dropdown-menu').toggleClass('-open');
+			let link = $(this).find('> .nav-link');
+			if(link.attr('aria-expanded') == "true"){
+				link.attr('aria-expanded', "false");
+			} else {
+				link.attr('aria-expanded', "true");
+			}
 		});
 	}
 };
