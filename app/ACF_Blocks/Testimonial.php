50cc2c5d209e050b969dc45dc34bc108bf616e1e
Alice Williams <alice@10degrees.uk>
2019-11-14 15:18:48 +0000

    Add options to testimonial block.

--- app/ACF_Blocks/Testimonial.php
+++ app/ACF_Blocks/Testimonial.php
@@ -6,15 +6,15 @@ use App\ACF_Blocks\AbstractBlockRegistration;
 
 class Testimonial extends AbstractBlockRegistration
 {
-	/**
-	 *
-	 * Declare ACF blocks for Block Editor
-	 * 
-	 */
-	public function register() 
-	{
-		acf_register_block(
-           [
+    /**
+     *
+     * Declare ACF blocks for Block Editor
+     *
+     */
+    public function register()
+    {
+        acf_register_block(
+            [
                 'name'				=> 'testimonial',
                 'title'				=> __('Testimonial'),
                 'description'		=> __('A custom testimonial block.'),
@@ -22,23 +22,23 @@ class Testimonial extends AbstractBlockRegistration
                 'category'			=> 'common',
                 'icon'				=> 'admin-comments',
                 'keywords'			=> array( 'testimonial', 'quote' ),
-           ]
+            ]
         );
-	}
-	
-	/**
-	 *
-	 * Callback to render ACF blocks
-	 * 
-	 */
-	public function render( $block ) 
-	{
-		// convert name ("acf/testimonial") into path friendly slug ("testimonial")
-		$slug = str_replace('acf/', '', $block['name']);
-		
-		// include a template part from within the "views/blocks" folder
-		if( file_exists( get_theme_file_path("/partials/blocks/{$slug}.php") ) ) {
-			include( get_theme_file_path("/partials/blocks/{$slug}.php") );
-		}
-	}
+    }
+
+    /**
+     *
+     * Callback to render ACF blocks
+     *
+     */
+    public function render($block)
+    {
+        // convert name ("acf/testimonial") into path friendly slug ("testimonial")
+        $slug = str_replace('acf/', '', $block['name']);
+        
+        // include a template part from within the "views/blocks" folder
+        if (file_exists(get_theme_file_path("/partials/blocks/{$slug}.php"))) {
+            include(get_theme_file_path("/partials/blocks/{$slug}.php"));
+        }
+    }
 }
