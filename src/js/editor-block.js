3c051593e5fd546715412814c1fde41bf949b0df
Alice Williams <alice@10degrees.uk>
2019-12-11 15:45:03 +0000

    Add hover styles for outlined button.

--- src/js/editor-block.js
+++ src/js/editor-block.js
@@ -1,6 +1,6 @@
 wp.domReady(() => {
   wp.blocks.unregisterBlockStyle("core/button", "default");
-  wp.blocks.unregisterBlockStyle("core/button", "outline");
+  // wp.blocks.unregisterBlockStyle("core/button", "outline");
   wp.blocks.unregisterBlockStyle("core/button", "squared");
   wp.blocks.unregisterBlockStyle("core/button", "fill");
 
