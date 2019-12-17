0464bf9cbdbda112a9fb7f3084ccf923e7eca871
Alice Williams <alice@10degrees.uk>
2019-12-17 16:47:49 +0000

    Rename variables and use the CSS custom properties.

--- partials/blocks/accordion.php
+++ partials/blocks/accordion.php
@@ -1,12 +1,12 @@
 <?php
-    $controller_id = uniqid('accordion-button-', true);
-    $panel_id = uniqid('accordion-content-', true);
+    $toggle_id = uniqid('accordion-toggle-', true);
+    $content_id = uniqid('accordion-content-', true);
 ?>
 
 <div <?php td_block_class($block, 'accordion'); ?>>
     <div class="container">
-        <p><button class="toggle" aria-expanded="false" id="<?php echo $controller_id; ?>" aria-controls="<?php echo $panel_id; ?>"><?php the_field('heading'); ?></button></p>
-        <div class="content" id="<?php echo $panel_id; ?>" aria-labelledby="<?php echo $controller_id; ?>">
+        <p><button class="toggle" aria-expanded="false" id="<?php echo $toggle_id; ?>" aria-controls="<?php echo $content_id; ?>"><?php the_field('heading'); ?></button></p>
+        <div class="content" id="<?php echo $content_id; ?>" aria-labelledby="<?php echo $toggle_id; ?>">
             <?php the_field('content'); ?>
         </div>
     </div>
