cddaaee1c1a56da104d8069d2cdfcc9e442051ca
Karl Maw <karl@10degrees.uk>
2020-01-06 11:41:04 +0000

    CLI : Complete auto-adding block class to block service provider & add partial creation

--- app/Boot/CliCommands.php
+++ app/Boot/CliCommands.php
@@ -36,48 +36,76 @@ class CliCommands
      */
     public function block($args, $assoc_args)
     {
-        //Save for later...
+        /**
+         * Setup vars for dicrectory and block names
+         */
         $path = trailingslashit(get_template_directory());
-
         $blockName = $args[0];
-        $blockClassName = ucfirst($args[0]);
+        $blockClassNameParts = explode('-', $args[0]);
+        $blockClassName = '';
+        foreach ($blockClassNameParts as $part) {
+            $blockClassName .= ucfirst($part);
+        }
         $blockTitle = strtolower($blockName);
 
-        // //Check if block already exists! (app/ACF_Blocks)
-        // if (file_exists($path.'app/ACF_Blocks/'.$blockClassName.'.php')) {
-        //     \WP_CLI::error( 'Block file already exists. Get outta here!', false);
-        // }
+        /**
+         * Create block
+         */
+         /**
+         * Check if block already exists! (app/ACF_Blocks)
+         */
+        if (file_exists($path.'app/ACF_Blocks/'.$blockClassName.'.php')) {
+            \WP_CLI::error( 'Block file already exists. Get outta here!', false);
+        }
 
-        // //Create register block in app/ACF_Blocks
-        // $template = file_get_contents($path.'partials/cli-templates/Block.php');
-        // $template = preg_replace('/__BLOCK_CLASSNAME__/', $blockClassName, $template);
-        // $template = preg_replace('/__BLOCK_TITLE__/', $blockTitle, $template);
-        // $template = preg_replace('/__BLOCK_NAME__/', $blockTitle, $template);
-        // $result = file_put_contents($path.'app/ACF_Blocks/'.$blockClassName.'.php',  $template);
+        /**
+         * Create register block in app/ACF_Blocks
+         */
+        $template = file_get_contents($path.'partials/cli-templates/Block.php');
+        $template = preg_replace('/__BLOCK_CLASSNAME__/', $blockClassName, $template);
+        $template = preg_replace('/__BLOCK_TITLE__/', $blockTitle, $template);
+        $template = preg_replace('/__BLOCK_NAME__/', $blockTitle, $template);
+        $result = file_put_contents($path.'app/ACF_Blocks/'.$blockClassName.'.php',  $template);
 
-        // //Reference in block service provider
+        if($result) {
+            \WP_CLI::line( 'Block registration class added' );
+        }
+
+        /**
+         * Reference in block service provider
+         */
+
         $block_service_provider_file_contents = file_get_contents($path.'app/Providers/BlockServiceProvider.php');
-        // $pattern = "'\\App\\ACF_Blocks\\".$blockClassName."'";
 
-        // $result = preg_match($pattern, $block_service_provider_file_contents);
+        $pattern = "'\\App\\ACF_Blocks\\".$blockClassName."'";
+        $result = preg_match($pattern, $block_service_provider_file_contents);
 
-        // if(!$result) {
-        //     \WP_CLI::error('Block already registered. Get outta here!');
-        // } else {
-        //     \WP_CLI::error('No match in providers', false);
-        $pattern = "/];/";
-
-        $replace = "]]]]]]";
-
-        $block_service_provider_file_contents = preg_replace("/];/", "]]]]]]", $block_service_provider_file_contents);
-
-
-        $result = file_put_contents($path.'app/Providers/BlockServiceProvider.php', $block_service_provider_file_contents);
-        
-            // }
+        if ($result) {
+            \WP_CLI::error('Block already registered. Get outta here! [1]');
+        } else {
+            $pattern = "/];/";
+            $replace = "    '\\App\\ACF_Blocks\\".$blockClassName."',";
+            $replace .= "\n    ];";
+            $block_service_provider_file_contents = preg_replace("/];/", $replace, $block_service_provider_file_contents);
+            $result = file_put_contents($path.'app/Providers/BlockServiceProvider.php', $block_service_provider_file_contents);
+            \WP_CLI::line('Block registration class referenced in BlockServiceProvider'); 
+        }
         
         // //Create partial
-        // file_put_contents();
+        $partial_contents = file_get_contents($path.'partials/cli-templates/block-partial.php');
+        $partial_contents = preg_replace('/__BLOCK_NAME__/', $blockTitle, $partial_contents);
+        $result = file_put_contents($path.'partials/blocks/'.$blockTitle.'.php', $partial_contents);
+
+        if ($result) {
+            \WP_CLI::line( 'Partial created' );
+        }
+
+
+
+        \WP_CLI::confirm( 'Would you like SCSS?', $assoc_args_scss = array() );
+        \WP_CLI::line( $assoc_args_scss[0]);
+
+
 
         // //Add SCSS file & reference in common
         // file_put_contents();
