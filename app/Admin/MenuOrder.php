39e8d04a73da541d01f1ae2ae6bdd8abac2838c9
Baber Parweez <baber@10degrees.uk>
2020-01-10 10:17:56 +0000

    reorder wpengine menu item

--- app/Admin/MenuOrder.php
+++ app/Admin/MenuOrder.php
@@ -33,6 +33,7 @@ class MenuOrder
         return array(
             'index.php',
             'separator1',
+            'admin.php?page=wpengine-common',
             'edit.php',
             'edit.php?post_type=page'
         );
