d41ca4656cb8b9b819d35e8c6810f62c440b8e2a
Alice Williams <alice@10degrees.uk>
2021-06-08 16:33:45 +0100

    Remove ignore and add docblock to functions.php. Fixes #38

--- functions.php
+++ functions.php
@@ -1,5 +1,15 @@
 <?php
-// phpcs:ignoreFile
+
+/**
+ * Initial WordPress entry point
+ *
+ * @category Theme
+ * @package  TenDegrees/10degrees-base
+ * @author   10 Degrees <wordpress@10degrees.uk>
+ * @license  https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html GPL-2.0+
+ * @link     https://github.com/10degrees/10degrees-base
+ * @since    2.0.0
+ */
 
 /**
  * If this file is accessed directory, then abort.
