d9b4a72fb23369480379e8fed53b739abd453246
Alice Williams <alice@10degrees.uk>
2021-06-08 09:35:42 +0100

    Use session storage to only show the IE modal once.

--- partials/ie-modal.php
+++ partials/ie-modal.php
@@ -1,4 +1,4 @@
-<div x-data="td_ie_modal()" x-init="checkForIE" x-show="showModal" class="modal-overlay">
+<div x-cloak x-data="td_ie_modal()" x-init="init" x-show="showModal" class="modal-overlay">
     <div class="modal">
         <h1><?php _e('IE Support', '@textdomain'); ?></h1>
         <p><?php _e("This site doesn't support Internet Explorer, so some pages might not look correct and some functionality might not work as expected.", '@textdomain'); ?></p>
--- src/js/alpine/ie-modal.js
+++ src/js/alpine/ie-modal.js
@@ -1,12 +1,40 @@
 window.td_ie_modal = function() {
     return {
         showModal: false,
-        checkForIE: function() {
+        init: function() {
+            // Only show for IE users
+            if (!this.isIE()) {
+                return;
+            }
+
+            // Only show once per session
+            if(this.hasSeenModal()){
+                return;
+            }
+
+            this.showModal = true;
+            this.setShownIEModal();
+        },
+        hasSeenModal: function(){
+            return window.sessionStorage.getItem('shownIEModal');
+        },
+        setShownIEModal: function(){
+            window.sessionStorage.setItem('shownIEModal', true);
+        },
+
+        /**
+         * Is the user using IE?
+         * 
+         * @link https://stackoverflow.com/a/49986758
+         * 
+         * @return  {boolean}  True if using IE, else false
+         */
+        isIE: function() {
             const ua = window.navigator.userAgent; //Check the userAgent property of the window.navigator object
             const msie = ua.indexOf('MSIE '); // IE 10 or older
             const trident = ua.indexOf('Trident/'); //IE 11
             
-            this.showModal = msie > 0 || trident > 0;
+            return (msie > 0 || trident > 0);
         }
     }
 }
