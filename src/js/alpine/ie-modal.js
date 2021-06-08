a5544c89c764e79e3c1b2a452387062eade82d4e
Alice Williams <alice@10degrees.uk>
2021-06-08 09:28:00 +0100

    Add check for IE.

--- src/js/alpine/ie-modal.js
+++ src/js/alpine/ie-modal.js
@@ -2,6 +2,11 @@ window.td_ie_modal = function() {
     return {
         showModal: false,
         checkForIE: function() {
+            const ua = window.navigator.userAgent; //Check the userAgent property of the window.navigator object
+            const msie = ua.indexOf('MSIE '); // IE 10 or older
+            const trident = ua.indexOf('Trident/'); //IE 11
+            
+            this.showModal = msie > 0 || trident > 0;
         }
     }
 }
