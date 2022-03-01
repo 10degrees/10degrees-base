f507fa42ce9d8ad156cf59f3cba7e99dc3afa6de
Alice Williams <alice@10degrees.uk>
2022-03-01 12:33:56 +0000

    Remove esc_html_e as the 404 content is a WYSIWYG.

--- 404.php
+++ 404.php
@@ -19,7 +19,7 @@
     get_template_part('partials/page', 'header');
 
     if ($option_text = get_option('options_404_error_page_text')) :
-        esc_html_e($option_text);
+        echo $option_text;
     else :
         echo '<div>';
         esc_html_e('Sorry, that page does not exist.', '@textdomain');
